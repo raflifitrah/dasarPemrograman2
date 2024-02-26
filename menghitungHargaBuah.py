c6e1ee386b3b8c0a50a9e55b1801b4901644526b
raflifitrah <143487172+raflifitrah@users.noreply.github.com>
2024-02-26 11:01:45 +0700

    Add files via upload

---
+++ menghitungHargaBuah.py
@@ -0,0 +1,15 @@
+jeruk = 15000
+apel = 30000
+mangga = 20000
+manggis  = 7000
+durian = 50000
+
+jumlah_jeruk = 3
+jumlah_apel = 2
+jumlah_mangga = 0
+jumlah_manggis = 5
+jumlah_durian = 2
+
+total = (jeruk  * jumlah_jeruk) + (apel  * jumlah_apel) + (mangga  * jumlah_mangga) + (manggis * jumlah_manggis) + (durian * jumlah_durian)
+
+print("total belanja anda adalah: %.0f " % total)
---
+++ menghitungUangKembalian.py
@@ -0,0 +1,6 @@
+uangBelanja = int(input("Masukkan uang belanja anda: "))
+totalBelanja = int(input("Masukkan total belanja anda: "))
+
+totalKembalian = uangBelanja-totalBelanja
+
+print("total kembalian anda adalah: %.0f" % totalKembalian)
